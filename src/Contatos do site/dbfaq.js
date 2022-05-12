6666be027f5971a76a24b1e5fbaa19ed6094ba61
Gabriela Abran <gabrielabc2@gmail.com>
2022-05-12 01:09:35 -0300

    Continuar desenv tela

---
+++ src/Contatos do site/dbfaq.js
@@ -0,0 +1,17 @@
+var db = {
+    faq: [
+        {
+            Pergunta: 'O que é uma operadora de plano de saúde?',
+            Resposta: 'Trata-se da pessoa jurídica que disponibiliza para venda e administra os planos privados de assistência à saúde.'
+        }, {
+            Pergunta: 'O que é um plano de saúde?',
+            Resposta: 'É um contrato individual ou coletivo que garante a prestação de serviços médicos e odontológicos para assistência à saúde, por tempo indeterminado e sem teto máximo, conforme contratado pelo segurado ou empresa. Os planos de saúde no Brasil são regulamentados pela Agência Nacional de Saúde Suplementar (ANS).'
+        }, {
+            Pergunta: 'O que é plano individual ou familiar?',
+            Resposta: 'O plano considerado individual é aquele contratado diretamente por uma pessoa física junto à operadora. Os membros da família podem participar do plano como dependentes, conforme definido no contrato'
+        }, {
+            Pergunta: 'O que é plano coletivo?',
+            Resposta: 'Os planos coletivos são contratados por pessoas jurídicas para funcionários, estatutários e profissionais pertencentes a conselhos, sindicatos, entidades de classe, etc. O plano coletivo pode ser empresarial ou por adesão.'
+        }
+    ]
+}
