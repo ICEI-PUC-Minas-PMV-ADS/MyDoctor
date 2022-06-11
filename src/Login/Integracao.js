86a05807f3804e90fd336abc58a023e768435109
Brunodesenvolvedor <emailprofissionaldobruno@gmail.com>
2022-06-11 18:52:36 -0300

    atualizacao

--- src/Login/Integracao.js
+++ src/Login/Integracao.js
@@ -8,12 +8,14 @@
 // }
 // })
 
-function entrar(){
+function entrar() {
+    /* 
+    console.log('Entrou')
     let usuario = document.querySelector('#email')
-//  let userlabel = document.querySelector('#userlabel')
+    //  let userlabel = document.querySelector('#userlabel')
 
     let senhalog = document.querySelector('#senha')
-//  let senhalabel = document.querySelector('#senhalabel')
+    //  let senhalabel = document.querySelector('#senhalabel')
 
     let msgError = document.querySelector('#msgError')
     let listaUser = []
@@ -25,41 +27,41 @@ function entrar(){
         cidade: '',
         endereco: '',
         estado: '',
-        esp: '', 
+        esp: '',
         crm: '',
-        senha:''
+        senha: ''
     }
 
     listaUser = JSON.parse(localStorage.getItem('listaUser'))
-    listaUser.forEach((item) =>{​
-if(usuario.value == item.emailcad && senhalog.value == item.senhacad){​
-uservalid = {​
-nome:item.nomecad,
-user:item.emailcad,
-celular:item.celularcad,
-cidade:item.cidadecad,
-endereco:item.enderecocad,
-estado:item.estadocad,
-esp:item.especad,
-crm:item.crmcad,
-senha:item.senhacad
+    listaUser.forEach((item) => {
+        if (usuario.value == item.emailcad && senhalog.value == item.senhacad) {
+            uservalid = {
+                nome: item.nomecad,
+                user: item.emailcad,
+                celular: item.celularcad,
+                cidade: item.cidadecad,
+                endereco: item.enderecocad,
+                estado: item.estadocad,
+                esp: item.especad,
+                crm: item.crmcad,
+                senha: item.senhacad
 
-        }​
-       }​
-   }​)
-if(usuario.value == uservalid.user && senhalog.value == uservalid.senha){​
-window.location.href='teste.html'
-window.alert (`Olá`)
-// lettoken = Math.random().toString(16).substring(2)
-// localStorage.setItem('token', token)
-// localStorage.setItem('userlogado', JSON.stringify(uservalid))
-   }​ else{​
-// userlabel.setAttribute('style', 'color:red')
-// usuario.setAttribute('style', 'border-color:red')
-// senhalabel.setAttribute('style', 'color:red')
-// senhalog.setAttribute('style', 'border-color:red')
-msgError.setAttribute('style', 'display:block')
-msgError.innerHTML = 'Usuário ou senha incorretos'
-usuario.focus()
-   }​
-}​
+            }
+        }
+    })
+    if (usuario.value == uservalid.user && senhalog.value == uservalid.senha) {
+        window.location.href = 'teste.html';
+        window.alert(`Olá`)
+        // lettoken = Math.random().toString(16).substring(2)
+        // localStorage.setItem('token', token)
+        // localStorage.setItem('userlogado', JSON.stringify(uservalid))
+    } else {
+        // userlabel.setAttribute('style', 'color:red')
+        // usuario.setAttribute('style', 'border-color:red')
+        // senhalabel.setAttribute('style', 'color:red')
+        // senhalog.setAttribute('style', 'border-color:red')
+        msgError.setAttribute('style', 'display:block')
+        msgError.innerHTML = 'Usuário ou senha incorretos'
+        usuario.focus()
+    } */
+​}
