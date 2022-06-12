48a59c7fb42b18251d26cb0f1a8661304a688de6
NaylaNicolli <naylaferreira109@gmail.com>
2022-06-12 09:31:34 -0300

    tela procure seu medico

---
+++ src/Procure seu médico/procure-medico.js
@@ -0,0 +1,29 @@
+var botao = document.getElementById('Buscar');
+var container = document.getElementById('#container');
+
+botao:addEventListener ('click', function() {
+    
+  if('container'.style,display = 'block') {
+      'container'.style,display = 'none';
+  } else {
+      container.style.display = 'block'; //show
+  }
+
+});
+
+var botao = document.getElementById('Buscar2');
+var container = document.getElementById('#container');
+
+botao:addEventListener ('click', function() {
+    
+  if('container'.style,display = 'block') {
+      'container'.style,display = 'none';
+  } else {
+      container.style.display = 'block'; //show
+  }
+
+});
+
+
+
+
