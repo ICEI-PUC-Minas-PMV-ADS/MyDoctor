70da379464ec5dbcb1dd4e7a582777d157a73f49
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-06-12 18:24:45 -0300

    Validar campos cidade e especialidade

--- src/Procure seu médico/procure-medico.js
+++ src/Procure seu médico/procure-medico.js
@@ -1,9 +1,17 @@
-var botaoBuscarCidade = document.getElementById('botao-buscar-cidade');
-var botaoBuscarEspecialidade = document.getElementById('botao-buscar-especialidade');
+var botaoBuscarCidade = document.querySelector('#botao-buscar-cidade');
+var botaoBuscarEspecialidade = document.querySelector('#botao-buscar-especialidade');
 var container = document.getElementById('container');
+
+let cidadeInvalida = document.querySelector("#cidade-invalida");
+let especialidadeInvalida = document.querySelector("#especialidade-invalida");
 
 let especialidade;
 let cidade;
+
+window.onload = () => {
+  cidadeInvalida.style.display = "none";
+  especialidadeInvalida.style.display = "none";
+}
 
 const changeCidade = event => {
   cidade = event.target.value;
@@ -13,12 +21,21 @@ const changeEspecialidade = event => {
   especialidade = event.target.value;
 }
 
-botaoBuscarRegiao.addEventListener('click', function() {
+botaoBuscarCidade.addEventListener("click", () => {
+  if (cidade.length < 3) {
+    cidadeInvalida.innerHTML = "O campo 'Cidade' está inválido. Deve ter no mínimo 3 caracteres.";
+    cidadeInvalida.style.display = "block";
+    cidadeInvalida.style.color = "red";
+  }
 });
 
-botaoBuscarEspecialidade.addEventListener('click', function() {
+botaoBuscarEspecialidade.addEventListener("click", () => {
+  if (especialidade.length < 5) {
+    especialidadeInvalida.innerHTML = "O campo 'Especialidade' está inválido. Deve ter no mínimo 5 caracteres.";
+    especialidadeInvalida.style.display = "block";
+    especialidadeInvalida.style.color = "red";
+  }
 });
-
 
 
 
