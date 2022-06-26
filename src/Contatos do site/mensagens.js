9f65f37820b0081ff6622649f4fcd65d10d5ea2b
Débora Gonçalves <deboracristinapereira123@gmail.com>
2022-06-25 23:13:23 -0300

    FIX: Limpar formulário após envio da mensagem - Página de contato

--- src/Contatos do site/mensagens.js
+++ src/Contatos do site/mensagens.js
@@ -1,15 +1,26 @@
-var listaDadosUsuarios = new Array()
+var listaMensagensUsuarios = new Array();
+
 function enviarMensagem(event) {
-    event.preventDefault()
+    event.preventDefault();
+
     var nome = document.getElementById("nomeUsuario").value;
     var email = document.getElementById("emailUsuario").value;
     var mensagem = document.getElementById("mensagemUsuario").value;
+
     var dadosUsuario = {
         nome: nome,
         email: email,
         mensagem: mensagem
-
     };
-    listaDadosUsuarios.push(dadosUsuario)
-    localStorage.setItem('listaDadosUsuarios', JSON.stringify(listaDadosUsuarios))
+
+    listaMensagensUsuarios.push(dadosUsuario);
+    localStorage.setItem('listaMensagensUsuarios', JSON.stringify(listaMensagensUsuarios));
+
+    limparFormulario();
+}
+
+function limparFormulario() {
+    document.getElementById("nomeUsuario").value = "";
+    document.getElementById("emailUsuario").value = "";
+    document.getElementById("mensagemUsuario").value = "";
 }
